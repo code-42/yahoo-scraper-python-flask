f26ba376cfd225cdb772ab097df9c08f4eba5323
Edward Dupre <edwd42@gmail.com>
2018-12-29 10:17:10 -0500

    move templates and app.py and forms.y into /scraper

---
+++ run.py
@@ -0,0 +1,13 @@
+# source youtube video https://www.youtube.com/watch?v=MwZwr5Tvyxo
+
+# set environment variables: export FLASK_APP=app.py
+# set environment variables: export FLASK_DEBUG=1
+
+from scraper import app
+
+# this only needed to run directly using python prompt >>>
+# set FLASK_APP=app
+# set FLASK_DEBUG=true
+# python -m flask run --port 3000
+if __name__ == '__main__':
+    app.run(debug=True)
