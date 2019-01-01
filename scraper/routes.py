cc230c11d2b822ad29aaec96f7507c0c04baa7fb
Edward Dupre <edwd42@gmail.com>
2019-01-01 17:22:52 -0500

    add hashed_password to registration form and commit to database

--- scraper/routes.py
+++ scraper/routes.py
@@ -34,11 +34,23 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        # source https://docs.python.org/2/library/hashlib.html
+        hashed_password = generate_password_hash(form.password.data)
+        print(hashed_password)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login", methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'dd@dd.com' and form.password.data == 'dd':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
