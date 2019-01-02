48eb2b62ba78d78b4a0e513a8244204092a7caeb
Edward Dupre <edwd42@gmail.com>
2019-01-01 19:51:08 -0500

    add LoginManager and UserMixin methods to models and routes

--- scraper/models.py
+++ scraper/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from scraper import db, login_manager
+from flask_login import UserMixin
 
 
 @login_manager.user_loader
@@ -8,7 +9,7 @@ def load_user(user_id):
 
 
 # sqlite database for users
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(60), unique=True, nullable=False)
--- scraper/routes.py
+++ scraper/routes.py
@@ -3,6 +3,7 @@ from scraper import app, db
 from scraper.forms import RegistrationForm, LoginForm
 from scraper.models import Totals, Watchlist, User
 from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import login_user
 
 
 # dummy data for scraper
@@ -50,9 +51,10 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'dd@dd.com' and form.password.data == 'dd':
-            flash('You have been logged in!', 'success')
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             return redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash('Login Unsuccessful. Please check email and password.', 'danger')
     return render_template('login.html', title='Login', form=form)
